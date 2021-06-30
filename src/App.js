21b2d0955cd279ab62295f8694df14e23c28de7a
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-30 15:51:19 -0400

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={'/Projects'}>
+            <Projects/>
+          </Route>
+          <Route exact path={'/Home'}>
+            <Home/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
