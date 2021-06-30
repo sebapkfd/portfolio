ed550f8c9f4598373c57a73ffdfe922dd10fac2e
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-30 17:11:15 -0400

    Added links to SNS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 const Home = () => {
+
     return (
         <div>
             <h1>Home</h1>
+            <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">Github</a>
+            <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer"> Linkedin</a>
         </div>
     )
 }
