aa7c3b38e2b5c8bbdec0b63b3e3e4ee3165f779b
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-30 16:16:41 -0400

    Implemented basic PDF share

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import CV from '../assets/CV.pdf';
 
 const Navbar = () => {
     return (
@@ -9,10 +10,9 @@ const Navbar = () => {
             <Link to={'/Projects'}>
                 <p>Projects</p>
             </Link>
-            <Link to ={'../src/assets/Sebastián Soto-CV.pdf'} target="_blank" rel="noopener noreferrer"> 
+            <Link to ={CV} target="_blank" rel="noopener noreferrer"> 
                 <p>Resume</p>
             </Link>
-            <a href ={'../assets/Sebastián Soto-CV.pdf'} target="_blank" >Resume</a>
         </div>
     )
 }
