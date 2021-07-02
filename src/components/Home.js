59b2ffc077f967b1619c97e16e6bc925d414167d
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-02 19:50:15 -0400

    Added small about me section

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,14 +14,10 @@ const Home = () => {
             <p>About me</p>
 
             <span>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
-                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, 
-                pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, 
-                vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede 
-                mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
-                Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, 
-                feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies 
-                nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+                Hi, I'm a developer interested in Full Stack Web Development. I have been studying programming since 2017 and I’m willing to keep learning new languages, 
+                tools, and follow the path of Web Development. As a goal I want to become a Senior Developer able to guide work teams and share ideas with others.
+
+                Among my strengths are discipline, interest for coding, high self teaching capability, and eagerness to learn from others.
             </span>
 
             <Skills />
