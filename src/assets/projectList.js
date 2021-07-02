8a7456bab4fd4a9cc8e112024a455bdaf7478ba2
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-01 20:58:51 -0400

    Added technologies used on projects array

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -4,42 +4,48 @@ const projectList = [
         repo: 'https://github.com/sebapkfd/shopping-cart',
         link: 'https://sebapkfd.github.io/shopping-cart/',
         id: '1',
-        showOnHome: true
+        showOnHome: true,
+        techUsed: ['js', 'react', 'css', 'html', 'git', 'npm']
     },
     {
         name: 'Battleship',
         repo: 'https://github.com/sebapkfd/battleship',
         link: 'https://sebapkfd.github.io/battleship/',
         id: '2',
-        showOnHome: true
+        showOnHome: true,
+        techUsed: ['js', 'react', 'css', 'html', 'git', 'npm', 'jest']
     },
     {
         name: 'Weather App',
         repo: 'https://github.com/sebapkfd/Weather-app',
         link: 'https://sebapkfd.github.io/Weather-app/',
         id: '3',
-        showOnHome: true
+        showOnHome: true,
+        techUsed: ['js', 'react', 'css', 'html', 'git', 'npm']
     },
     {
         name: 'CV project',
         repo: 'https://github.com/sebapkfd/cv-project',
         link: 'https://sebapkfd.github.io/cv-project/',
         id: '4',
-        showOnHome: false
+        showOnHome: false,
+        techUsed: ['js', 'react', 'css', 'html', 'git', 'npm']
     },
     {
         name: 'Todo list',
         repo: 'https://github.com/sebapkfd/todolist',
         link: 'https://sebapkfd.github.io/todolist/',
         id: '5',
-        showOnHome: false
+        showOnHome: false,
+        techUsed: ['js', 'webpack', 'css', 'html', 'git']
     },
     {
         name: 'Memory Card Game',
         repo: 'https://github.com/sebapkfd/memory-card',
         link: 'https://sebapkfd.github.io/memory-card/',
         id: '6',
-        showOnHome: false
+        showOnHome: false,
+        techUsed: ['js', 'react', 'css', 'html', 'git', 'npm']
     }
 ]
 
