cac413b7bfdd99b69b890b323053a1642a0df960
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-03 22:06:03 -0400

    Removed title from project item

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,7 +3,6 @@ const ProjectItem = (props) => {
 
     return (
         <div className='project-item'>
-            <h1>Project Item</h1>
             <h2>{project.name}</h2>
             <a href={project.link} target='_blank' rel="noopener noreferrer">Live demo</a>
             <a href={project.repo} target='_blank' rel="noopener noreferrer">Repository</a>
