6ce232a872923b0ffeedee556e97bf758c130a5e
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-03 20:58:41 -0400

    Changing references

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,30 +6,32 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-            <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">Github</a>
-            <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer"> Linkedin</a>
-            <p>Email: sebastian.pkfd@gmail.com</p>
+            <h1>About me</h1>
+            <div>
+                <h1 id='about' href='about'>About me</h1>
+                <span>
+                    Hi, I'm a Full Stack Web Developer. I have been studying programming since 2017 and I’m willing to keep learning new languages, 
+                    tools. As a goal I want to become a Senior Developer able to guide teams and share ideas with others.
+                    Among my strengths are discipline, interest for coding, high self teaching capability, and eagerness to learn from others.
+                </span>
+            </div>
 
-            <p>About me</p>
+            <p id='skills' href='skills'>Skills</p>
+            <Skills/>
 
-            <span>
-                Hi, I'm a Full Stack Web Developer. I have been studying programming since 2017 and I’m willing to keep learning new languages, 
-                tools. As a goal I want to become a Senior Developer able to guide teams and share ideas with others.
-
-                Among my strengths are discipline, interest for coding, high self teaching capability, and eagerness to learn from others.
-            </span>
-
-            <Skills />
-            <h1>Projects</h1>
-
-            {projectList.map(pr => {
-                return (pr.showOnHome) ? <ProjectItem key={pr.id} project={pr} /> : null;
-            })}
-
-            <Link to={'/portfolio/projects'}>
-                <button>See more</button>
-            </Link>
+            <div>
+                <h1 id='projects' href='projects'>Projects</h1>
+                {projectList.map(pr => {
+                    return (pr.showOnHome) ? <ProjectItem key={pr.id} project={pr} /> : null;
+                })}
+            </div>
+            
+            <div>
+                <h1 id='contact' href='contact'>Contact</h1>
+                <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">Github</a>
+                <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer"> Linkedin</a>
+                <p>Email: sebastian.pkfd@gmail.com</p>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,13 @@
-import { Link } from 'react-router-dom';
 import CV from '../assets/CV.pdf';
 
 const Navbar = () => {
     return (
         <div id={'navbar'}>
-            <Link to ={'/portfolio'}>
-                <p>Home</p>
-            </Link>
-            <Link to={'/portfolio/projects'}>
-                <p>Projects</p>
-            </Link>
-            <Link to ={CV} target="_blank" rel="noopener noreferrer"> 
-                <p>Resume</p>
-            </Link>
+            <a href="#about">About</a>
+            <a href='#skills'>Skills</a>
+            <a href='#projects'>Projects</a>
+            <a href='#contact'>Contact</a>
+            <a href={CV} target="_blank" rel="noopener noreferrer">Resume</a>
         </div>
     )
 }
