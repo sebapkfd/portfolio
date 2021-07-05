15a6cf7f23551572f61c0c89c0f5d1e844a7b0a1
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-04 23:42:04 -0400

    Changed details in about me

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
         <div className='section' id='about' href='about'>
             <h1>About me</h1>
             <span>
-                Hi, I'm a Full Stack Web Developer. I have been studying programming since 2017 and I’m willing to keep learning new languages, 
+                Hi, I'm a Full Stack Web Developer. I have been programming since 2017 and I’m willing to keep learning new languages and 
                 tools. As a goal I want to become a Senior Developer able to guide teams and share ideas with others.
                 Among my strengths are discipline, interest for coding, high self teaching capability, and eagerness to learn from others.
             </span>
