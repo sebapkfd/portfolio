dfa577751d16b48cddd6c9d85d406081cc2c5f6f
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-05 18:31:20 -0400

    Added basic positioning in About me

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 const About = () => {
     return (
         <div className='section' id='about' href='about'>
-            <h1>About me</h1>
-            <span>
-                Hi, I'm a Full Stack Web Developer. I have been programming since 2017 and I’m willing to keep learning new languages and 
-                tools. As a goal I want to become a Senior Developer able to guide teams and share ideas with others.
-                Among my strengths are discipline, interest for coding, high self teaching capability, and eagerness to learn from others.
-            </span>
+            <div>
+            <img src="https://img.icons8.com/android/240/000000/google-code.png"/>
+            </div>
+            <div id='about__info'>
+                <h1>About me</h1>
+                <span>
+                    Hi, I'm a Full Stack Web Developer. I have been programming since 2017 and I’m willing to keep learning new languages and 
+                    tools.
+                </span>
+                {/* <p>As a goal I want to become a Senior Developer able to guide teams and share ideas with others.
+                    Among my strengths are discipline, interest for coding, high self teaching capability, and eagerness to learn from others.
+                </p> */}
+            </div>
         </div>
     )
 }
