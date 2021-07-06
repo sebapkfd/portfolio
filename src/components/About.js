678c5fe1773157479509146af5b3db6de67cf3d5
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-06 01:19:13 -0400

    Added classnames and removed unnused code from about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,15 @@
 const About = () => {
     return (
         <div className='section' id='about' href='about'>
-            <div>
-            <img src="https://img.icons8.com/android/240/000000/google-code.png"/>
+            <div className='about__colums'>
+                <img src="https://img.icons8.com/android/240/000000/google-code.png"/>
             </div>
-            <div id='about__info'>
+            <div id='about__info' className='about__colums'>
                 <h1>About me</h1>
                 <span>
                     Hi, I'm a Full Stack Web Developer. I have been programming since 2017 and I’m willing to keep learning new languages and 
                     tools.
                 </span>
-                {/* <p>As a goal I want to become a Senior Developer able to guide teams and share ideas with others.
-                    Among my strengths are discipline, interest for coding, high self teaching capability, and eagerness to learn from others.
-                </p> */}
             </div>
         </div>
     )
