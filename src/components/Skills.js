f6feb1703259f208fa8c6ce35fc73e634cb43e57
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-06 01:49:39 -0400

    Added basic grid position to skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,28 +2,30 @@ const Skills = () => {
     return (
         <div className='section' id='skills' href='skills'>
             <h1>Skills</h1>
-            <div>
-                <h2>Front end</h2>
-                <p>Javascript</p>
-                <p>React</p>
-                <p>HTML</p>
-                <p>CSS</p>
-                <p>Jest</p>
-            </div>
-            <div>
-                <h2>Back end</h2>
-                <p>Node</p>
-                <p>Express</p>
-                <p>MongoDB</p>
-                <p>Mongoose</p>
-            </div>
-            <div>
-                <h2>Other tools</h2>
-                <p>Git</p>
-                <p>NPM</p>
-                <p>Heroku</p>
-                <p>Github</p>
-                <p>Webpack</p>
+            <div id='skills-table'>
+                <div className='skill-item'>
+                    <h2>Front end</h2>
+                    <p>Javascript</p>
+                    <p>React</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                    <p>Jest</p>
+                </div>
+                <div className='skill-item'>
+                    <h2>Back end</h2>
+                    <p>Node</p>
+                    <p>Express</p>
+                    <p>MongoDB</p>
+                    <p>Mongoose</p>
+                </div>
+                <div className='skill-item'>
+                    <h2>Other tools</h2>
+                    <p>Git</p>
+                    <p>NPM</p>
+                    <p>Heroku</p>
+                    <p>Github</p>
+                    <p>Webpack</p>
+                </div>
             </div>
         </div>
     )
