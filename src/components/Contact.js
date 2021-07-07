481c38ac5c2301756de6c5f35e3f8f88b697dc27
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-07 01:24:48 -0400

    Added logos to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
+import github from '../assets/github-logo.png';
+import linkedin from '../assets/linkedin-logo.png';
+import email from '../assets/email-logo.png';
+
 const Contact = () => {
     return (
         <div className='section' id='contact' href='contact'>
             <h1>Contact</h1>
-            <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">Github</a>
-            <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer"> Linkedin</a>
-            <p>Email: sebastian.pkfd@gmail.com</p>
+            <div id='contact__links'>
+                <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
+                    <img src={github} alt='github-logo'/>
+                </a>
+                <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
+                    <img src={linkedin} alt='linkedin-logo'/>
+                </a>
+            </div>
+            <div>
+                <img src={email} alt='email-logo'/>
+                <h2>sebastian.pkfd@gmail.com</h2>
+            </div>
         </div>
     )
 }
