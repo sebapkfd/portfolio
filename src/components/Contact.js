5e7c378cdaa21a30793813e978362b3df943d1a0
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-07 01:44:39 -0400

    Added basic style to contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,17 +6,19 @@ const Contact = () => {
     return (
         <div className='section' id='contact' href='contact'>
             <h1>Contact</h1>
-            <div id='contact__links'>
-                <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
-                    <img src={github} alt='github-logo'/>
-                </a>
-                <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
-                    <img src={linkedin} alt='linkedin-logo'/>
-                </a>
-            </div>
-            <div>
-                <img src={email} alt='email-logo'/>
-                <h2>sebastian.pkfd@gmail.com</h2>
+            <div id='contact-info'>
+                <div id='contact__links'>
+                    <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
+                        <img src={github} alt='github-logo'/>
+                    </a>
+                    <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
+                        <img src={linkedin} alt='linkedin-logo'/>
+                    </a>
+                </div>
+                <div id='contact__email'>
+                    <img src={email} alt='email-logo'/>
+                    <h2>sebastian.pkfd@gmail.com</h2>
+                </div>
             </div>
         </div>
     )
