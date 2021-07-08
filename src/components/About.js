9d0940ff4a6c6639c7742864cd4d8bd33ccf3cca
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-08 17:15:12 -0400

    Small hover effects added

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,9 @@ const About = () => {
                 <img src="https://img.icons8.com/android/240/000000/google-code.png" alt='main-logo'/>
             </div>
             <div id='about__info' className='about__colums'>
-                <h1>About me</h1>
+                <h1>Hi, I’m Sebastian Soto</h1>
                 <span>
-                    Hi, my name is Sebastian Soto and I am a Full Stack Web Developer. I have been programming since 2017 and I’m willing to keep learning new languages and tools.
+                    I am a Full Stack Web Developer, I have been programming since 2017 and I’m willing to keep learning new languages and tools.
                 </span>
             </div>
         </div>
