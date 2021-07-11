d119585b1a5ac097723d570bad5e61939586946a
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-10 23:35:48 -0400

    Added basic responsiveness to navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 
 function App() {
     return (
@@ -12,7 +12,7 @@ function App() {
             <About/>
             <Skills/>
             <Projects/>
-            <Contact/>
+            {/* <Contact/> */}
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import CV from '../assets/CV.pdf';
 
 const Navbar = () => {
+    const [screen, setScreen] = useState('navbar');
+
+    const toggleNavbar = () => {
+        (screen === 'navbar') ? setScreen('responsive') : setScreen('navbar');
+    }
+
+    const hideNavbar = () => {
+        if (screen === 'responsive') {
+            setScreen('navbar')
+        }
+    }
+
     return (
-        <div id={'navbar'}>
-            <a href="#about">About</a>
-            <a href='#skills'>Skills</a>
-            <a href='#projects'>Projects</a>
-            <a href='#contact'>Contact</a>
-            <a href={CV} target="_blank" rel="noopener noreferrer">Resume</a>
+        <div className={screen}>
+            <a href="#about" onClick={() => hideNavbar()}>About</a>
+            <a href='#skills' onClick={() => hideNavbar()}>Skills</a>
+            <a href='#projects' onClick={() => hideNavbar()}>Projects</a>
+            {/* <a href='#contact' onClick={() => hideNavbar()}>Contact</a> */}
+            <a href={CV} target="_blank" rel="noopener noreferrer" onClick={() => hideNavbar()}>Resume</a>
+            <p id="navbar__control" onClick={() => toggleNavbar()}>More</p>
         </div>
     )
 }
