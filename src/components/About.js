1fee86de8dbcb0bc9bdcf156c46d94fd572c66da
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-13 01:36:35 -0400

    Added prior style ot about me and improved content

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,15 @@
 import github from '../assets/github-logo.png';
 import linkedin from '../assets/linkedin-logo.png';
-import email from '../assets/email-logo.png';
 
 const About = () => {
     return (
         <div className='section' id='about' href='about'>
-            <h1>Contact</h1>
+            <h1>About me</h1>
             <span>
             I have been coding since 2017 when I started my career. In 2020 I started learning self-taught Web Development by building projects and following The Odin Project curriculum, since then I have learned new tools, languages and frameworks everyday.
-            Let's get in contact, you can write at sebastian.pkfd@gmail.com.
-            You can also find me in:
+            Let's get in contact, you can write me at sebastian.pkfd@gmail.com.
             </span>
+            <p>You can also find me in:</p>
             <div id='contact-info'>
                 <div id='contact__links'>
                     <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
@@ -19,10 +18,6 @@ const About = () => {
                     <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
                         <img src={linkedin} alt='linkedin-logo'/>
                     </a>
-                </div>
-                <div id='contact__email'>
-                    <img src={email} alt='email-logo'/>
-                    <h2>sebastian.pkfd@gmail.com</h2>
                 </div>
             </div>
         </div>
