3ccf83c54f9e1d9c4bf57e2c8cd23f1705429524
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-13 18:30:56 -0400

    Refactored About structure and added copy email to clipboard functionality

--- src/components/About.js
+++ src/components/About.js
@@ -2,23 +2,27 @@ import github from '../assets/github-logo.png';
 import linkedin from '../assets/linkedin-logo.png';
 
 const About = () => {
+
+    const copyMail = () => {
+        navigator.clipboard.writeText('sebastian.pkfd@gmail.com');
+    }
+
     return (
         <div className='section' id='about' href='about'>
             <h1>About me</h1>
             <span>
-            I have been coding since 2017 when I started my career. In 2020 I started learning self-taught Web Development by building projects and following The Odin Project curriculum, since then I have learned new tools, languages and frameworks everyday.
-            Let's get in contact, you can write me at sebastian.pkfd@gmail.com.
+            I have been coding since 2017 when I started my career. In 2020 I started learning self-taught Web Development by building projects and following The Odin Project curriculum, since then I have been learning new tools, languages and frameworks everyday.
             </span>
+            <p>Let's get in contact, you can write me at <button onClick={() => copyMail()}>sebastian.pkfd@gmail.com</button></p>
+            
             <p>You can also find me in:</p>
-            <div id='contact-info'>
-                <div id='contact__links'>
-                    <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
-                        <img src={github} alt='github-logo'/>
-                    </a>
-                    <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
-                        <img src={linkedin} alt='linkedin-logo'/>
-                    </a>
-                </div>
+            <div id='contact__links'>
+                <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
+                    <img src={github} alt='github-logo'/>
+                </a>
+                <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
+                    <img src={linkedin} alt='linkedin-logo'/>
+                </a>
             </div>
         </div>
     )
