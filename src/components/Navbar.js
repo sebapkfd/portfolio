35edbb029f76822467e753f04e7d0494a62ade5f
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-14 19:26:44 -0400

    Moved home button to left

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,12 +18,16 @@ const Navbar = () => {
 
     return (
         <div className={screen}>
-            <p id="navbar__control" onClick={() => toggleNavbar()}>{toggleText}</p>
-            <a href="#home" onClick={() => hideNavbar()}>Home</a>
-            <a href="#about" onClick={() => hideNavbar()}>About</a>
-            <a href='#skills' onClick={() => hideNavbar()}>Skills</a>
-            <a href='#projects' onClick={() => hideNavbar()}>Projects</a>
-            <a href={CV} target="_blank" rel="noopener noreferrer" onClick={() => hideNavbar()}>Resume</a>
+            <div id='navbar-left'>
+                <p id="navbar__control" onClick={() => toggleNavbar()}>{toggleText}</p>
+                <a id='home-button' href="#home" onClick={() => hideNavbar()}>Home</a>
+            </div>
+            <div id='navbar-right'>
+                <a href="#about" onClick={() => hideNavbar()}>About</a>
+                <a href='#skills' onClick={() => hideNavbar()}>Skills</a>
+                <a href='#projects' onClick={() => hideNavbar()}>Projects</a>
+                <a href={CV} target="_blank" rel="noopener noreferrer" onClick={() => hideNavbar()}>Resume</a>
+            </div>
         </div>
     )
 }
