dba7064fbdbb463e9ff62b132895c2dd567c56bf
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-16 15:59:02 -0400

    Changed 'write me at' for 'email me at'

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import github from '../assets/github-dark-logo.svg';
+import githubDark from '../assets/github-dark-logo.svg';
 import linkedin from '../assets/linkedin-logo.svg';
 
 const About = () => {
@@ -13,7 +13,7 @@ const About = () => {
             <p className='about-info'>
                 I have been coding since 2017 when I started my career. In 2020 I started learning self-taught Web Development by building projects and following The Odin Project curriculum, since then I have been learning new tools, languages and frameworks everyday.
             </p>
-            <p className='about-info'>Let's get in contact, you can write me at</p>
+            <p className='about-info'>Let's get in contact, email me at</p>
 
             <div className='mail-wrapper'>
                 <span className='tooltip'>Click to copy email addres</span>
@@ -22,7 +22,7 @@ const About = () => {
             <p>You can also find me in:</p>
             <div id='contact__links'>
                 <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
-                    <img src={github} alt='github-logo'/>
+                    <img src={githubDark} alt='github-logo'/>
                 </a>
                 <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
                     <img src={linkedin} alt='linkedin-logo'/>
